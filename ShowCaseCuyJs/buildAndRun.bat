b248b5d9f4bee7fd722cf6ce4143f4262b626ff8
unknown <Ronald Coarite>
2021-05-01 20:39:26 +0100

    mejoras

--- ShowCaseCuyJs/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t domolin/ShowCaseCuyJs .
-call docker rm -f ShowCaseCuyJs
-call docker run -d -p 9080:9080 -p 9443:9443 --name ShowCaseCuyJs domolin/ShowCaseCuyJs
